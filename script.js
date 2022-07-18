99a499aaff4f874af7f24a1b66022ef6e62c11cd
AHMED NABIB <nabibpallab@gmail.com>
2022-07-19 02:24:07 +0600

    revealing section

--- script.js
+++ script.js
@@ -239,6 +239,28 @@ headerObserver.observe(head);
 
 ///////////////////////////////////////
 
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
 // rgb(255,255,255)
 
 // const randomInt = (min, max) =>
